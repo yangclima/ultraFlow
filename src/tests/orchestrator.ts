4e3b3216f26527cbec1679bb68a39a58e136e228
yanlima <yangclima@gmail.com>
2026-03-07 13:00:53 -0300

    feat: add `runPendingMigrations` to `orchestrator.ts`

---
+++ src/tests/orchestrator.ts
@@ -0,0 +1,38 @@
+import retry from 'async-retry';
+import database from '@/infra/database';
+import migrationsRunner from '@/infra/migrations-runner';
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch('http://localhost:3000/api/v1/status');
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+}
+
+async function runPendingMigrations() {
+  await migrationsRunner.runPendingMigrations();
+}
+
+async function clearDatabase() {
+  await database.query('DROP schema public cascade; CREATE schema public;');
+}
+
+const orchestrator = {
+  waitForAllServices,
+  clearDatabase,
+  runPendingMigrations,
+};
+
+export default orchestrator;
